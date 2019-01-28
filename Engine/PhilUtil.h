0648ad814e5a4d31f7c8ce85417b976989a9ff51
Philipp <philipp.hoff@outlook.de>
2019-01-28 22:53:56 +0100

    new utilities file; output text file

---
+++ Engine/PhilUtil.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <sstream>
+
+namespace phil
+{
+	template<typename T>
+	std::string Crop(T in, int n)
+	{
+		std::stringstream s_in;
+		s_in << in;
+		std::string sstr = s_in.str();
+		while (sstr.length() < n)
+		{
+			sstr += " ";
+		}
+		while (sstr.length() > n)
+		{
+			sstr.pop_back();
+		}
+		return sstr;
+	}
+}
