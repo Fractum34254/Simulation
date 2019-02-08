8dd75340296fe8fa99d76a752f555ee34042dfb2
Philipp <philipp.hoff@outlook.de>
2019-02-08 10:22:31 +0100

    much better exceptions & fixed some bugs w/ test code

--- Engine/File.h
+++ Engine/File.h
@@ -30,8 +30,9 @@ public:
 		//test, if file exists																						|-> can lead to EXCEPTION
 		if(!file)
 		{
-			std::string info = "Can't open file ";	///standart-syntax
-			info += name;							///wrong filename
+			std::string info = "Can't open file \"";	///standart-syntax
+			info += name;								///wrong filename
+			info += "\"";
 			throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
 		}
 
@@ -50,12 +51,19 @@ public:
 
 		///reading out the settings
 		bool endReached = false;
-		std::string setting;
 
 		do {
+			std::string setting;
 			///reading out the name of a setting && testing for end													|-> can lead to EXCEPTION
 			for (char c = file.get(); !endReached && !isInVector(settingNames, setting); c = file.get())
 			{
+				if (file.eof())
+				{
+					std::string info = "Not enough data in file \"";				///standart-syntax
+					info += name;													///file with incorrect settings
+					info += "\": Reached end of file";
+					throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
+				}
 				setting += c;
 				if (setting.size() > std::max(getLongest(settingNames), settingsEnd.size()))
 				{
@@ -71,14 +79,14 @@ public:
 				}
 			}
 			///now name of setting is in 'setting'
-			if (!endReached)
+			if (!endReached && !file.eof())
 			{
 				///if init is already true, setting was already initialized											|-> can lead to EXCEPTION
 				if (settingInit.at(findInVector(settingNames, setting)))
 				{
-					std::string info = "Double initialized setting in file ";		///standart-syntax
+					std::string info = "Double initialized setting in file \"";		///standart-syntax
 					info += name;													///file with incorrect settings
-					info += ": ";
+					info += "\": ";
 					info += setting;
 					throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
 				}
@@ -87,6 +95,7 @@ public:
 				///																									|-> ADD NEW SETTINGS HERE
 				if (setting == axisColorSet)
 				{
+					file.unget();
 					///red color value
 					std::string r;
 					int ri;
@@ -99,7 +108,7 @@ public:
 						}
 						file.unget();
 						///read out r
-						for (c = file.get(); c != ' '; c = file.get())
+						for (c = file.get(); (c != -1) && (c != ' '); c = file.get())
 						{
 							r += c;
 						}
@@ -107,12 +116,21 @@ public:
 						try
 						{
 							ri = std::stoi(r);
+							if (0 > ri || 255 < ri)
+							{
+								std::string info = "Bad red axis color value in file \"";
+								info += name;
+								info += "\":\n";
+								info += toString(ri);
+								info += " (not in range from 0 to 255)\n";
+								throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
+							}
 						}
 						catch (const std::exception&)
 						{
-							std::string info = "Bad red axis color value in file ";
+							std::string info = "Bad red axis color value in file \"";
 							info += name;
-							info += ": ";
+							info += "\": ";
 							info += r;
 							throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
 						}
@@ -130,7 +148,7 @@ public:
 						}
 						file.unget();
 						///read out r
-						for (c = file.get(); c != ' '; c = file.get())
+						for (c = file.get(); (c != -1) && (c != ' '); c = file.get())
 						{
 							g += c;
 						}
@@ -138,12 +156,21 @@ public:
 						try
 						{
 							gi = std::stoi(g);
+							if (0 > gi || 255 < gi)
+							{
+								std::string info = "Bad green axis color value in file \"";
+								info += name;
+								info += "\":\n";
+								info += toString(gi);
+								info += " (not in range from 0 to 255)\n";
+								throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
+							}
 						}
 						catch (const std::exception&)
 						{
-							std::string info = "Bad green axis color value in file ";
+							std::string info = "Bad green axis color value in file \"";
 							info += name;
-							info += ": ";
+							info += "\": ";
 							info += g;
 							throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
 						}
@@ -161,7 +188,7 @@ public:
 						}
 						file.unget();
 						///read out r
-						for (c = file.get(); c != ' '; c = file.get())
+						for (c = file.get(); (c != -1) && (c != ' ') && (c != '\n'); c = file.get())
 						{
 							b += c;
 						}
@@ -169,12 +196,21 @@ public:
 						try
 						{
 							bi = std::stoi(b);
+							if (0 > bi || 255 < bi)
+							{
+								std::string info = "Bad blue axis color value in file \"";
+								info += name;
+								info += "\":\n";
+								info += toString(bi);
+								info += " (not in range from 0 to 255)\n";
+								throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
+							}
 						}
 						catch (const std::exception&)
 						{
-							std::string info = "Bad blue axis color value in file ";
+							std::string info = "Bad blue axis color value in file \"";
 							info += name;
-							info += ": ";
+							info += "\": ";
 							info += b;
 							throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
 						}
@@ -185,104 +221,11 @@ public:
 				}
 				else if (setting == graphColorSet)
 				{
+					file.unget();
 					///red color value
 					std::string r;
 					int ri;
 					{
-						///test for blank space
-						char c = file.get();
-						while (c == ' ')
-						{
-							c = file.get();
-						}
-						file.unget();
-						///read out r
-						for (c = file.get(); c != ' '; c = file.get())
-						{
-							r += c;
-						}
-						///if conversion fails, a std::exception is thrown -> catch & throw own exception with more informations
-						try
-						{
-							ri = std::stoi(r);
-						}
-						catch (const std::exception&)
-						{
-							std::string info = "Bad red graph color value in file ";
-							info += name;
-							info += ": ";
-							info += r;
-							throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
-						}
-					}
-					char rc = ri;
-					///green color value
-					std::string g;
-					int gi;
-					{
-						///test for blank space
-						char c = file.get();
-						while (c == ' ')
-						{
-							c = file.get();
-						}
-						file.unget();
-						///read out r
-						for (c = file.get(); c != ' '; c = file.get())
-						{
-							g += c;
-						}
-						///if conversion fails, a std::exception is thrown -> catch & throw own exception with more informations
-						try
-						{
-							gi = std::stoi(g);
-						}
-						catch (const std::exception&)
-						{
-							std::string info = "Bad green graph color value in file ";
-							info += name;
-							info += ": ";
-							info += g;
-							throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
-						}
-					}
-					char gc = gi;
-					///blue color value
-					std::string b;
-					int bi;
-					{
-						///test for blank space
-						char c = file.get();
-						while (c == ' ')
-						{
-							c = file.get();
-						}
-						file.unget();
-						///read out r
-						for (c = file.get(); c != ' '; c = file.get())
-						{
-							b += c;
-						}
-						///if conversion fails, a std::exception is thrown -> catch & throw own exception with more informations
-						try
-						{
-							bi = std::stoi(b);
-						}
-						catch (const std::exception&)
-						{
-							std::string info = "Bad blue graph color value in file ";
-							info += name;
-							info += ": ";
-							info += b;
-							throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
-						}
-					}
-					char bc = bi;
-					///assemble them in one color
-					pixelColor = Color(rc, gc, bc);
-				}
-				else if (setting == yNameSet)
-				{
 					///test for blank space
 					char c = file.get();
 					while (c == ' ')
@@ -290,7 +233,129 @@ public:
 						c = file.get();
 					}
 					file.unget();
-					for (c = file.get(); c != ' '; c = file.get())
+					///read out r
+					for (c = file.get(); (c != -1) && (c != ' '); c = file.get())
+					{
+						r += c;
+					}
+					///if conversion fails, a std::exception is thrown -> catch & throw own exception with more informations
+					try
+					{
+						ri = std::stoi(r);
+						if (0 > ri || 255 < ri)
+						{
+							std::string info = "Bad red graph color value in file \"";
+							info += name;
+							info += "\":\n";
+							info += toString(ri);
+							info += " (not in range from 0 to 255)\n";
+							throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
+						}
+					}
+					catch (const std::exception&)
+					{
+						std::string info = "Bad red graph color value in file \"";
+						info += name;
+						info += "\": ";
+						info += r;
+						throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
+					}
+				}
+					char rc = ri;
+					///green color value
+					std::string g;
+					int gi;
+					{
+					///test for blank space
+					char c = file.get();
+					while (c == ' ')
+					{
+						c = file.get();
+					}
+					file.unget();
+					///read out r
+					for (c = file.get(); (c != -1) && (c != ' '); c = file.get())
+					{
+						g += c;
+					}
+					///if conversion fails, a std::exception is thrown -> catch & throw own exception with more informations
+					try
+					{
+						gi = std::stoi(g);
+						if (0 > gi || 255 < gi)
+						{
+							std::string info = "Bad green graph color value in file \"";
+							info += name;
+							info += "\":\n";
+							info += toString(gi);
+							info += " (not in range from 0 to 255)\n";
+							throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
+						}
+					}
+					catch (const std::exception&)
+					{
+						std::string info = "Bad green graph color value in file \"";
+						info += name;
+						info += "\": ";
+						info += g;
+						throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
+					}
+				}
+					char gc = gi;
+					///blue color value
+					std::string b;
+					int bi;
+					{
+					///test for blank space
+					char c = file.get();
+					while (c == ' ')
+					{
+						c = file.get();
+					}
+					file.unget();
+					///read out r
+					for (c = file.get(); (c != -1) && (c != ' ') && (c != '\n'); c = file.get())
+					{
+						b += c;
+					}
+					///if conversion fails, a std::exception is thrown -> catch & throw own exception with more informations
+					try
+					{
+						bi = std::stoi(b);
+						if (0 > bi || 255 < bi)
+						{
+							std::string info = "Bad blue graph color value in file \"";
+							info += name;
+							info += "\":\n";
+							info += toString(bi);
+							info += " (not in range from 0 to 255)\n";
+							throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
+						}
+					}
+					catch (const std::exception&)
+					{
+						std::string info = "Bad blue graph color value in file \"";
+						info += name;
+						info += "\": ";
+						info += b;
+						throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
+					}
+				}
+					char bc = bi;
+					///assemble them in one color
+					pixelColor = Color(rc, gc, bc);
+				}
+				else if (setting == yNameSet)
+				{
+					file.unget();
+					///test for blank space
+					char c = file.get();
+					while (c == ' ')
+					{
+						c = file.get();
+					}
+					file.unget();
+					for (c = file.get(); (c != ' ') && (c != -1) && (c != '\n'); c = file.get())
 					{
 						yAxisName += c;
 					}
@@ -302,8 +367,9 @@ public:
 				///checks, if all settings are initialized															|-> can lead to EXCEPTION	
 				if (!std::all_of(settingInit.begin(), settingInit.end(), [](bool b) {return b; }))
 				{
-					std::string info = "Not enough settings found in ";
+					std::string info = "Not enough settings found in \"";
 					info += name;
+					info += "\"";
 					throw Exception(_CRT_WIDE(__FILE__), __LINE__, towstring(info));
 				}
 			}
@@ -383,5 +449,11 @@ private:
 			maxLength = std::max(maxLength, s.size());
 		}
 		return maxLength;
+	}
+	static std::string toString(int i)
+	{
+		std::stringstream ss;
+		ss << i;
+		return ss.str();
 	}
 };
