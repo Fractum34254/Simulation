9913b0a74b5aaf17602307b5d579538c57969844
Philipp <philipp.hoff@outlook.de>
2019-02-24 23:03:50 +0100

    now drawing all 5 (6) axis divisions

--- Engine/Graph.cpp
+++ Engine/Graph.cpp
@@ -62,7 +62,7 @@ void Graph::CoordinateSystem::Draw(const Font & f, Graphics & gfx, float xMaxAxi
 	///maximum number length for rect calculation later
 	size_t yNumberLength = 0;
 	///yAxis
-	for (int i = 1; i < 5; i++)
+	for (int i = 1; i <= 5; i++)
 	{
 		gfx.DrawLine(yAxis - lineLength / 2.0f, xAxis - i * deltaY, yAxis + lineLength / 2.0f, xAxis - i * deltaY, axisColor);
 		std::stringstream ss;
